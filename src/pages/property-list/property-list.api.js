3f8dd3ec036aac508a4485f17648665d3e5f6b6b
Gumidafe <devgumidafe@gmail.com>
2021-07-21 14:32:35 +0100

    feat (getSaleTypeList, getProviceList): métodos en donde nos traemos los tipos de ventas y las provincias del servidor

--- src/pages/property-list/property-list.api.js
+++ src/pages/property-list/property-list.api.js
@@ -1,13 +1,33 @@
 import axios from 'axios';
 
-const url = `${process.env.BASE_API_URL}/properties`;
+const url = `${process.env.BASE_API_URL}`;
 
-export const getPropertyList = () =>
-  axios.get(url)
+export const getPropertyList = () => {
+  return axios.get(`${url}/properties`)
     .then(response => {
       return response.data;
     })
     .catch(error => {
       console.log(error)
     })
+}
 
+export const getSaleTypeList = () => {
+  return axios.get(`${url}/saleTypes`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const getProvinceList = () => {
+  return axios.get(`${url}/provinces`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
--- src/pages/property-list/property-list.js
+++ src/pages/property-list/property-list.js
@@ -1,10 +1,16 @@
-import { getPropertyList } from './property-list.api';
+import { getPropertyList, getSaleTypeList, getProvinceList } from './property-list.api';
 import { mapPropertyListFromApiToVM } from './property-list.mappers';
-import { addPropertyRows } from './property-list.helpers';
+import { addPropertyRows, setOptions } from './property-list.helpers';
 
 getPropertyList().then(propertyList => {
   const viewModelPropertyList = mapPropertyListFromApiToVM(propertyList);
-
   addPropertyRows(viewModelPropertyList);
 })
 
+getSaleTypeList().then(saleTypeList => {
+  setOptions(saleTypeList, 'select-sale-type', '¿Qué venta?')
+})
+
+getProvinceList().then(provinceList => {
+  setOptions(provinceList, 'select-province', '¿Dónde?')
+})
