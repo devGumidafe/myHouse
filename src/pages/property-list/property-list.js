cf147befb371300ca0c3b86b55a4854829f8aeb5
Gumidafe <devgumidafe@gmail.com>
2021-07-22 11:20:26 +0100

    Rellenamos los select

--- src/pages/property-list/property-list.js
+++ src/pages/property-list/property-list.js
@@ -1,16 +1,25 @@
 import { getPropertyList, getSaleTypeList, getProvinceList } from './property-list.api';
 import { mapPropertyListFromApiToVM } from './property-list.mappers';
 import { addPropertyRows, setOptions } from './property-list.helpers';
+import { roomOptions, bathroomOptions, minPriceOptions, maxPriceOptions } from './property-list-constants';
 
 getPropertyList().then(propertyList => {
   const viewModelPropertyList = mapPropertyListFromApiToVM(propertyList);
   addPropertyRows(viewModelPropertyList);
-})
+});
 
 getSaleTypeList().then(saleTypeList => {
   setOptions(saleTypeList, 'select-sale-type', '¿Qué venta?')
-})
+});
 
 getProvinceList().then(provinceList => {
   setOptions(provinceList, 'select-province', '¿Dónde?')
-})
+});
+
+setOptions(roomOptions, 'select-room', '¿Habitaciones?');
+
+setOptions(bathroomOptions, 'select-bathroom', '¿Cuartos de baño?');
+
+setOptions(minPriceOptions, 'select-min-price', 'Min(EUR)');
+
+setOptions(maxPriceOptions, 'select-max-price', 'Max(EUR)');
